03db96439185ae9e218f4b49a03217162f602dea
TharunK116 <96480774+TharunK116@users.noreply.github.com>
2025-03-05 14:55:24 +0530

    Added ids to section config files

--- linkedin_/src/molecules/profileContent/Constants/sections.js
+++ linkedin_/src/molecules/profileContent/Constants/sections.js
@@ -2,31 +2,38 @@
 const SECTIONS = [
     {
         title: "Profile",
+        id:"PROFILE",
         type: "profile",
         fieldIds: ["profilePic", "name", "contact", "connections"],
     },
     {
         title: "Experience",
+        id: "EXPERIENCE",
         type: "experience",
         fieldIds: ["Company", "Position", "Description", "StartDate", "EndDate", "Location"],
     },
     {
         title: "Education",
+         id: "EDUCATION",
         type: "education",
         fieldIds: ["CollegeName", "Degree", "Branch", "StartDate", "EndDate", "Location"],
     },
     {
         title: "Skills",
+        id: "SKILLS",
+       
         type: "skills",
         fieldIds: ["SkillName", "Proficiency"],
     },
     {
         title: "Analytics",
-        type: "analytics",
+         id: "ANALYTICS",
+       
         fieldIds: ["metric", "value"],
     },
     {
         title: "Interests",
+         id: "INTERESTS",
         type: "interests",
         fieldIds: ["skillName", "proficiency"],
     },
